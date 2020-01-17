4c7e9bfd3156a087f1090def648d8e942f964c5f
“dziekan30” <dziekan30@gmail.com>
2020-01-17 11:31:36 -0600

    update

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -6,8 +6,39 @@ class Api::ProductsController < ApplicationController
       render 'index.json.jb'
     end 
 
+    def create
+      @products = Product.new(
+                              name: params[:name],
+                              price: params[:price],
+                              image_url: params[:image_url],
+                              description: params[:description]
+        )
+      @products.save
+      render 'show.json.jb'
+
+    end 
+
+
     def show
       @products = Product.find(params[:id])
       render "show.json.jb"
+    end
+
+    def update
+      @products = Product.find(params[:id])
+
+      @products.name = params[:name] || @products.name
+      @products.price = params[:price] || @products.price
+      @products.image_url = params[:image_url] || @products.image_url
+      @products.description = params[:description] || @products.description
+
+      @products.save
+      render "show.json.jb"
+    end
+
+    def destroy
+      products = Product.find(params[:id])
+      products.destroy
+      render json: {message: "Product successfully destroyed"}
     end
 end
