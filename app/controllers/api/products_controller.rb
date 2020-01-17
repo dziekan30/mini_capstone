009cb25f4abc6b0cd675101715ebd5b6991d8c28
“dziekan30” <dziekan30@gmail.com>
2020-01-17 10:07:55 -0600

    update

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -1,22 +1,13 @@
 class Api::ProductsController < ApplicationController
 
-    def all_products
+   
+    def index
       @products = Product.all
-      render "joker_basket.json.jb"
-    end
+      render 'index.json.jb'
+    end 
 
-
-    def first_product
-      @product = Product.first
-      render "first_product.json.jb"
-    end
-
-    def second_product
-      @product = Product.second
-      render "second_product.json.jb"
-    end
-      def third_product
-      @product = Product.third
-      render "third_product.json.jb"
+    def show
+      @products = Product.find(params[:id])
+      render "show.json.jb"
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Rails.application.routes.draw do
   namespace :api do
 
-    get '/all_products_url' => 'products#all_products'
-    get '/first_product_url' => 'products#first_product'
-    get '/second_product_url' => 'products#second_product'
-    get '/third_product_url' => 'products#third_product'
+
+    get '/products' => 'products#index'
+    get '/products/:id' => 'products#show'
   end
 end
